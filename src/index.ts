d62270125b89d9d2358a4528bdd10d81468498bb
Kyle <kyle@cloudversify.com>
2018-08-19 17:34:49 -0700

    echo server started. client connects

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,37 @@
 import * as WebSocket from "websocket";
 import * as http from "http";
 
-var WebSocketServer = WebSocket.server;
-console.log('server started. waiting for connections');
-var server = http.createServer(function(request, response) {
+const WebSocketServer = WebSocket.server;
+
+const server = http.createServer((request, response) => {
   // process HTTP request. Since we're writing just WebSockets
   // server we don't have to implement anything.
 });
-server.listen(1337, function() { });
+server.listen(1337, undefined);
 
 // create the server
-var wsServer = new WebSocketServer({
-  httpServer: server
+const wsServer = new WebSocketServer({
+  "httpServer": server
 });
 
 // WebSocket server
-wsServer.on('request', function(request) {
-  console.log('client connected')
-  var connection = request.accept(undefined, request.origin);
+wsServer.on("request", (request) => {
+  console.log("client connected");
+  const connection = request.accept(undefined, request.origin);
+  connection.send("hello world");
 
   // This is the most important callback for us, we'll handle
   // all messages from users here.
-  connection.on('message', function(message) {
-    if (message.type === 'utf8') {
-      console.log('got message: ', message.utf8Data)
+  connection.on("message", (message) => {
+    if (message.type === "utf8") {
+      console.log("got message: ", message.utf8Data);
       // process WebSocket message
     }
   });
 
-  connection.on('close', function(connection) {
+  connection.on("close", (connection) => {
     // close user connection
   });
 });
+
+console.log("server started. waiting for connections");
